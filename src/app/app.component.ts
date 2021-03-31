5deccea517bd8adad70f4a8476309be61f667df1
DONGMO BERNARD GERAUD <keenndjc@gmail.com>
2021-03-31 22:57:13 +0100

    calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,73 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {}
+}*/
+
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+
+import { Platform, AlertController, IonRouterOutlet } from '@ionic/angular';
+import { Router} from '@angular/router';
+import {Location} from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent {
+
+  @ViewChildren(IonRouterOutlet) routerOutlets: QueryList < IonRouterOutlet > ;
+
+  lastTimeBackPress = 0;
+  timePeriodToExit = 2000;
+
+  
+
+  constructor(
+    private platform: Platform,
+    private alertController: AlertController,
+    private router: Router,
+    private location: Location
+  ) {
+    this.backButtonEvent();
+  }
+
+
+  backButtonEvent() {
+    this.platform.backButton.subscribeWithPriority(0, () => {
+      this.routerOutlets.forEach(async(outlet: IonRouterOutlet) => {
+        if (this.router.url != '/home') {
+          // await this.router.navigate(['/']);
+          await this.location.back();
+        } else if (this.router.url === '/home') {
+          if (new Date().getTime() - this.lastTimeBackPress >= this.timePeriodToExit) {
+            this.lastTimeBackPress = new Date().getTime();
+            this.presentAlertConfirm();
+          } else {
+            navigator['app'].exitApp();
+          }
+        }
+      });
+    });
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      // header: 'Confirm!',
+      message: 'Are you sure you want to exit the app?',
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: (blah) => {}
+      }, {
+        text: 'Close App',
+        handler: () => {
+          navigator['app'].exitApp();
+        }
+      }]
+    });
+  
+    await alert.present();
+  }
 }
